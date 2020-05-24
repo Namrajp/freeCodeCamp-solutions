9c7bbccc2a55f37629a2c9f44983dccbe9860cdd
Nammaraj <active99raj@gmail.com>
2020-05-24 15:55:13 +1000

    initial commit

---
+++ Basic-Javascript/word-blanks.js
@@ -0,0 +1,13 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+
+const wordBlanks = (myNoun, myAdjective, myVerb, myAdverb) => {
+const result = `This nice ${myNoun} + is very cool and  ${myAdjective}. Yesterday he ${myVerb} very ${myAdverb} to avoid stumble with my running run`; 
+	return result;
+};
+
+wordBlanks('dog','big','ran','quickly');
