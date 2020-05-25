e3f7bfd786e0d3b8bcbd11bbfa7d5e75ba9f08c6
Nava_mystic <active99raj@gmail.com>
2020-05-25 13:55:52 +1000

    changed file extentions

---
+++ Basic-Javascript/Basic-js-programs.js
@@ -0,0 +1,87 @@
+$ find . -name "*.txt" | xargs sed 's/.txt/.js/g' *.txt
+// Setup
+var myArray = [];
+for (let i=9; i>0; i-=2) {
+  myArray.push(i);
+}
+// Only change code below this line
+// Setup
+var myArray = [];
+for (let i=1; i<10; i+=2) {
+  myArray.push(i)
+}
+// Only change code below this line
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+for(let i=0; i<myArr.length; i++) {
+  total+= myArr[i];
+}
+// Only change code below this line
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+}
+while (i < 11);
+// Setup
+var myArray = [];
+for (let i=1; i<6; i++) {
+  myArray.push(i)
+}
+// Only change code below this line
+// Setup
+var myArray = [];
+var i= 5;
+ while (i >= 0) {
+   myArray.push(i)
+   i--;
+ }
+// Only change code below this line
+// Setup
+var myArray = [];
+for (let i=9; i>0; i-=2) {
+  myArray.push(i);
+}
+// Only change code below this line
+// Setup
+var myArray = [];
+for (let i=1; i<10; i+=2) {
+  myArray.push(i)
+}
+// Only change code below this line
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total = 0;
+for(let i=0; i<myArr.length; i++) {
+  total+= myArr[i];
+}
+// Only change code below this line
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+}
+while (i < 11);
+// Setup
+var myArray = [];
+for (let i=1; i<6; i++) {
+  myArray.push(i)
+}
+// Only change code below this line
+// Setup
+var myArray = [];
+var i= 5;
+ while (i >= 0) {
+   myArray.push(i)
+   i--;
+ }
+// Only change code below this line
